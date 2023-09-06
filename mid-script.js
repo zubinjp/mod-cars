ad4f8cff438a60020d1a76af3b919b730676217f
Abbeyjp <>
2023-09-06 08:46:35 +0000

    Added the values for the contact sections

--- mid-script.js
+++ mid-script.js
@@ -1,7 +1,15 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
+function on(n) {
+    if (n == 1) {
+        document.getElementById("").style.display = "block";
+    }
+    Polymer({
+        is: "overlay",
+        properties: {
+            name: String,
+            description: String
+        }
+    });
 }
-
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
