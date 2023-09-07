7abc22a2f24db3b66817afc846ccebbcc2460e82
Replit user <>
2023-09-07 11:41:40 +0000

    Completing the website

--- mid-script.js
+++ mid-script.js
@@ -6,102 +6,186 @@ function on(n) {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
 function carlisted(n) {
-  var pth = "";
-  var txt1 = "";
-  txt1 = "testtttttttttttttttttttttt";
+  const pth = [];
   if (n == 1) {
 
-    pth = "assets/images/xm_cart_page/bentley-8-litre-1931.jpg";
-    txt1 = "Just arrived with us...A beautiful immensely powerful 8 Litre Le Mans style tourer. This car was extensively built up by noted specialists & marque experts NDR Racing who sold the car previosly to the current owner. Absolutely everything has been done to this car with a full engine rebuild & chassis rebuild to like new spec. Since then The car has covered a scant 2000 miles in the sunny climes of Marbella Spain where the current owner has a hoilday home. The car started life as a Thrupp & Maberly landaulette ordered new by one Mrs Ossorio who took delivery in March of 1932. It suffered a fire in the 1950's after which it was rescued & rebuilt into the wonderful tourer we see today. The owner informs us that has been utterly reliable during his tenure & has matching components including chassis, engine axles & F type gear box. An immense long distance Le Mans style fuel tank is installed allowing for very long distance non stop trips. The green fabric body is like new as is the green leather interior with racing style bucket seats.To the rear is a comfortable bench seat.The cars' engine breathes though a triple carb set up giving it very spirited performance indeed with the ability to pass modern cars at motorway speeds with ease. The brakes are strong & progressive as might be expected form a car set up by NDR. The dash is equipped with a full quota of period instrumentaton including large rev counter & speedometer in addition to the various other guages.Twin fuel pumps are fitted activated by separate switches to the right of the steering wheel.The car rides on 21inch wire whels with knock off hubs, shod with brand new michelin tyres extending to the two side mounted spares.Wire covered headlamps give an imposing frontal appeaance of a full on Le Mans style car.A large Fish Tail exhaust completes the ensemble at the other end.The car has a full length new mohair hood under its custom hood bag but we did not put it up & neither do we expect you to.";
+    pth[0] = "assets/images/xm_cart_page/bentley-8-litre-1931.jpg";
+    pth[1] = "1931 Bentley 8 Litre";
+    pth[2]="id number=#958A4625";
+    pth[3] = "Year of manufacturing: 1945";
+    pth[4] = "Drive: Left-Hand drive";
 
 
   }
   else if (n == 2) {
 
-    pth = "assets/images/xm_cart_page/cadillac-fleetwood-eldorado.jpg";
-    txt1 = "testtttttttttttttttttttttt";
+    pth[0] = "assets/images/xm_cart_page/cadillac-fleetwood-eldorado.jpg";
+    pth[1] = "Cadillac-fleetwood Eldorado";
+    pth[2]="id number=#958B4625";
+    pth[3] = "Year of manufacturing: 1967";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 3) {
 
-    pth = "assets/images/xm_cart_page/cadillac1957.jpg";
+    pth[0] = "assets/images/xm_cart_page/cadillac1957.jpg";
+    pth[1] = "Cadillac 1957";
+    pth[2]="id number=#958G4625";
+    pth[3] = "Year of manufacturing: 1960";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 4) {
 
-    pth = "assets/images/xm_cart_page/chevrolet-3100.jpg";
-    txt1 = "testtttttttttttttttttttttt";
+    pth[0] = "assets/images/xm_cart_page/chevrolet-3100.jpg";
+    pth[1] = "Chevrolet 3100";
+    pth[2]="id number=#958L4625";
+    pth[3] = "Year of manufacturing: 1975";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 5) {
 
-    pth = "assets/images/xm_cart_page/chevrolet-gm-1963.jpg";
+    pth[0] = "assets/images/xm_cart_page/chevrolet-gm-1963.jpg";
+    pth[1] = "Chevrolet GM 1963";
+    pth[2]="id number=#958Z4625";
+    pth[3] = "Year of manufacturing: 1975";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 6) {
 
-    pth = "assets/images/xm_cart_page/citroen-traction-avant-6-big.jpg";
+    pth[0] = "assets/images/xm_cart_page/citroen-traction-avant-6-big.jpg";
+    pth[1] = "Citroen Traction Avant 6-big";
+    pth[2]="id number=#958U4625";
+    pth[3] = "Year of manufacturing: 1975";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 7) {
 
-    pth = "assets/images/xm_cart_page/Citroën-Traction-Avant-11-BL-1947.jpg";
+    pth[0] = "assets/images/xm_cart_page/Citroën-Traction-Avant-11-BL-1947.jpg";
+    pth[1] = "Citroen Traction Avant 11 BL";
+    pth[2]="id number=#958A9925";
+    pth[3] = "Year of manufacturing: 1960";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 8) {
 
-    pth = "assets/images/xm_cart_page/dodge-charger.jpg";
+    pth[0] = "assets/images/xm_cart_page/dodge-charger.jpg";
+    pth[1] = "Dodge Charger";
+    pth[2]="id number=#958S4625";
+    pth[3] = "Year of manufacturing: 2008";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 9) {
 
-    pth = "assets/images/xm_cart_page/fiat-500d-1968.jpg";
+    pth[0] = "assets/images/xm_cart_page/fiat-500d-1968.jpg";
+    pth[1] = "1968 Fiat 500D";
+    pth[2]="id number=#958V4625";
+    pth[3] = "Year of manufacturing: 1979";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 10) {
 
-    pth = "assets/images/xm_cart_page/fiat-zastava-z-750.jpg";
+    pth[0] = "assets/images/xm_cart_page/fiat-zastava-z-750.jpg";
+    pth[1] = "Fiat Zastava Z-750";
+    pth[2]="id number = #958H4625";
+    pth[3] = "Year of manufacturing: 1991";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 11) {
 
-    pth = "assets/images/xm_cart_page/ford_mustang_gt350.jpg";
+    pth[0] = "assets/images/xm_cart_page/ford_mustang_gt350.jpg";
+    pth[1] = "Ford Mustang GT350";
+    pth[2]="id number=#958K4625";
+    pth[3] = "Year of manufacturing: 2010";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 12) {
 
-    pth = "assets/images/xm_cart_page/ford-f1000.jpg";
+    pth[0] = "assets/images/xm_cart_page/ford-f1000.jpg";
+    pth[1] = "Ford F1000";
+    pth[2]="id number=#958W4625";
+    pth[3] = "Year of manufacturing: 1988";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 13) {
 
-    pth = "assets/images/xm_cart_page/jaguar-e-type-1961.jpg";
+    pth[0] = "assets/images/xm_cart_page/jaguar-e-type-1961.jpg";
+    pth[1] = "Jaguar E-type 1961";
+    pth[2]="id number=#958I4625";
+    pth[3] = "Year of manufacturing: 1970";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 14) {
 
-    pth = "assets/images/xm_cart_page/lincoln-cont-mark-III.jpg";
+    pth[0] = "assets/images/xm_cart_page/lincoln-cont-mark-III.jpg";
+    pth[1] = "Lincoln Continental Mark-III";
+    pth[2]="id number=#958P1625";
+    pth[3] = "Year of manufacturing: 1965";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 15) {
 
-    pth = "assets/images/xm_cart_page/lincoln-continental-mark-IV.jpg";
+    pth[0] = "assets/images/xm_cart_page/lincoln-continental-mark-IV.jpg";
+     pth[1] = "Lincoln Continental Mark-IV";
+    pth[2]="id number=#958U9625";
+    pth[3] = "Year of manufacturing: 1985";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 16) {
 
-    pth = "assets/images/xm_cart_page/mercedes-benz-w123.jpg";
+    pth[0] = "assets/images/xm_cart_page/mercedes-benz-w123.jpg";
+    pth[1] = "Mercedes Benz W123";
+    pth[2]="id number=#958F4625";
+    pth[3] = "Year of manufacturing: 1991";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 17) {
 
-    pth = "assets/images/xm_cart_page/moskvitch-408.jpg";
+    pth[0] = "assets/images/xm_cart_page/moskvitch-408.jpg";
+    pth[1] = "Moskvitch 408";
+    pth[2]="id number=#954X4625";
+    pth[3] = "Year of manufacturing: 1977";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 18) {
 
-    pth = "assets/images/xm_cart_page/renault-r4-1981.jpg";
+    pth[0] = "assets/images/xm_cart_page/renault-r4-1981.jpg";
+    pth[1] = "Renault R4 1981";
+    pth[2]="id number=#958C4625";
+    pth[3] = "Year of manufacturing: 1988";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 19) {
 
-    pth = "assets/images/xm_cart_page/toyota-mr2.jpg";
+    pth[0] = "assets/images/xm_cart_page/toyota-mr2.jpg";
+    pth[1] = "Toyota-MR2";
+    pth[2]="id number=#950S8625";
+    pth[3] = "Year of manufacturing: 1985";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 20) {
 
-    pth = "assets/images/xm_cart_page/vaz-2101.jpg";
+    pth[0] = "assets/images/xm_cart_page/vaz-2101.jpg";
+     pth[1] = "Vaz 2101";
+    pth[2]="id number=#958K2325";
+    pth[3] = "Year of manufacturing: 1965";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 21) {
 
-    pth = "assets/images/xm_cart_page/volkswagen-ifa-f9.jpg";
+    pth[0] = "assets/images/xm_cart_page/volkswagen-ifa-f9.jpg";
+    pth[1] = "Volkswagen IFA F9";
+    pth[2]="id number=#958R4625";
+    pth[3] = "Year of manufacturing: 1975";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else if (n == 22) {
 
-    pth = "assets/images/xm_cart_page/volkswagen-karmann-ghia.jpg";
+    pth[0] = "assets/images/xm_cart_page/volkswagen-karmann-ghia.jpg";
+    pth[1] = "Volkswagen Karmann Ghia";
+    pth[2]="id number=#448A4625";
+    pth[3] = "Year of manufacturing: 1965";
+    pth[4] = "Drive: Left-Hand drive";
   }
   else {
     off();
@@ -109,9 +193,11 @@ function carlisted(n) {
 
   }
 
-  document.getElementById("carimg").src = pth;
-  document.getElementById("carspec").innerHTML = txt1;
-  document.getElementById("carspec").innerHTML = txt1;
+  document.getElementById("carimg").src = pth[0];
+  document.getElementById("carname").innerHTML = pth[1];
+  document.getElementById("prodidnum").innerHTML = pth[2];
+  document.getElementById("yrmnf").innerHTML = pth[3];
+  document.getElementById("carspec").innerHTML = pth[4];
 
 }
 
